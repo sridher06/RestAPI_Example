fecc945cc5021a5bb45f25b06e24c6aeeef88d44
sabhis231 <sabhis231@gmail.com>
2023-05-01 18:45:47 +0100

    Fix done for Inital run

--- src/main/java/com/sri/tech/HelloWorld.java
+++ src/main/java/com/sri/tech/HelloWorld.java
@@ -7,19 +7,14 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-
-
 @Path("/api")
 public class HelloWorld {
 	
-		 	    
-		    @GET
-		    @Path("/hello")
-		    @Produces(MediaType.APPLICATION_JSON)
-		    public String sayHello() {
-		    	Date dt= new Date();
-		        return "Hellow world  RESTful Web Services SE Bootstrap"+dt;
-		    }
-		    
-		   
+	@GET
+    @Path("/hello")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String sayHello() {
+    	Date dt= new Date();
+        return "Hellow world  RESTful Web Services SE Bootstrap:  "+dt;
+    }
 }
--- src/main/java/com/sri/tech/HelloWorldApplication.java
+++ src/main/java/com/sri/tech/HelloWorldApplication.java
@@ -1,30 +1,18 @@
 package com.sri.tech;
-
-
-
-
 import java.util.HashSet;
 import java.util.Set;
 
 import jakarta.ws.rs.ApplicationPath;
-
 import jakarta.ws.rs.core.Application;
 
-	@ApplicationPath("/")
-	public class HelloWorldApplication extends Application {
-
-		/*
-		 * public HelloWorldApplication() {
-		 * System.out.println("start -HelloWorldApplication "); }
-		 */
-
-	        @Override
-	        public Set<Class<?>> getClasses() {
-	        	 Set<Class<?>> classes = new HashSet<>();
-	        	 classes.add(HelloWorld.class);
-	            return classes;
-	        }
-
-	
-	}
+@ApplicationPath("/")
+public class HelloWorldApplication extends Application {
+	 
+	@Override
+    public Set<Class<?>> getClasses() {
+		Set<Class<?>> classes = new HashSet<>();
+    	 classes.add(HelloWorld.class);
+        return classes;
+     }
+}
 
